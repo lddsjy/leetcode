19b0885faaae60fc16ebbcc2c1d1221d48263f2c
710910630@qq.com <710910630@qq.com>
2020-02-25 18:55:44 +0800

    add new ways

--- replaceSpace.py
+++ replaceSpace.py
@@ -5,21 +5,26 @@
 #     def replaceSpace(self, s):
 #         arr = s.split()
 #         return "%20".join(arr)
+
 class Solution:
-    # s 源字符串
-    def replaceSpace(self, s):
-        # write code here
-        print(list(s))
-        pos = []
-        s = list(s)
-        for i in range(len(s)):
-            if s[i] == ' ':
-                s[i] = '%20'
-                #s= s[:i]+'%20'+ remain
-        news = ''
-        for i in range(len(s)):
-            news += s[i]
-        return news
+    def replaceSpace(self, s: str) -> str:
+        return s.replace(' ', '%20')
+
+# class Solution:
+#     # s 源字符串
+#     def replaceSpace(self, s):
+#         # write code here
+#         print(list(s))
+#         pos = []
+#         s = list(s)
+#         for i in range(len(s)):
+#             if s[i] == ' ':
+#                 s[i] = '%20'
+#                 #s= s[:i]+'%20'+ remain
+#         news = ''
+#         for i in range(len(s)):
+#             news += s[i]
+#         return news
 
 s = Solution()
 print(s.replaceSpace("   "))
